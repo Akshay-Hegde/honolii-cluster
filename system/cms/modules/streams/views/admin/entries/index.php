5750ea916014b0176198a1c4864c0a0aad374cf9
Adam Fairholm <adam.fairholm@gmail.com>
2011-12-10 16:12:46 -0500

    Check for array in entries index.

--- system/cms/modules/streams/views/admin/entries/index.php
+++ system/cms/modules/streams/views/admin/entries/index.php
@@ -20,7 +20,7 @@
 		<?php foreach ($data as $field => $data_item):?>
 			<tr>
 				<?php if($stream->sorting == 'custom'): ?><td width="30" class="handle"><?php echo image('icons/drag_handle.gif'); ?></td><?php endif; ?>
-				<?php foreach( $stream->view_options as $view_option ): ?>
+				<?php if(is_array($stream->view_options): foreach( $stream->view_options as $view_option ): ?>
 				<td>
 				
 				<input type="hidden" name="action_to[]" value="<?php echo $data_item->id;?>" />
@@ -42,7 +42,7 @@
 					endif;
 					
 				?></td>
-				<?php endforeach; ?>
+				<?php endforeach; endif; ?>
 				<td class="actions">
 				
 					<?php echo anchor('admin/streams/entries/edit/'.$stream->id.'/'.$data_item->id, 'Edit', 'class="button"'); ?>
