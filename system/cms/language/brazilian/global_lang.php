pyrocms/pyrocms

Translate some strings in page footer

system/cms/language/brazilian/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label'] 					= 'Instalar';
 $lang['enable_label']					= 'Ativar';
 $lang['disable_label'] 					= 'Desativar';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= 'Atalhos';
 $lang['global:description'] 		    = 'Descrição';
 $lang['global:actions']					= 'Ações';

system/cms/language/chinese_traditional/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label'] 					= '安裝'; 			//'Install';
 $lang['enable_label']					= '啟用'; 			//'Enable';
 $lang['disable_label'] 					= '禁用'; 			//'Disable';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= '捷徑';
 $lang['global:description']				= '說明';
 $lang['global:actions']					= '操作';

system/cms/language/english/global_lang.php
@@ -69,6 +69,11 @@ $lang['enable_label']					= 'Enable';
 $lang['disable_label'] 					= 'Disable';
 $lang['upgrade_label'] 					= 'Upgrade';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';
+$lang['top_page']						= 'Top';
+$lang['back_to_top']					= 'Back to Top';
+
 $lang['global:filters'] 				= 'Filters';
 $lang['global:shortcuts'] 				= 'Shortcuts';
 $lang['global:description']				= 'Description';

system/cms/language/german/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label'] 					= 'Installieren';
 $lang['enable_label']					= 'Aktivieren';
 $lang['disable_label'] 					= 'Deaktivieren';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= 'Shortcuts'; #translate 
 $lang['global:description']				= 'Beschreibung'; 
 $lang['global:actions']					= 'Aktion';

system/cms/language/greek/global_lang.php
@@ -68,6 +68,11 @@ $lang['enable_label']					= 'Ενεργοποίηση';
 $lang['disable_label'] 					= 'Απενεργοποίηση';
 $lang['upgrade_label'] 					= 'Ενημέρωση';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:filters'] 				= 'Φίλτρα';
 $lang['global:shortcuts'] 				= 'Συντομέυσεις';
 $lang['global:description'] 		    = 'Περιγραφή';

system/cms/language/italian/global_lang.php
@@ -67,6 +67,11 @@ $lang['enable_label']					= 'Abilita';
 $lang['disable_label'] 					= 'Disabilita';
 $lang['upgrade_label'] 					= 'Aggiorna';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= 'Collegamenti';
 $lang['global:description'] 		    = 'Descrizione';
 $lang['global:actions']					= 'Azioni';

system/cms/language/lithuanian/global_lang.php
@@ -67,6 +67,11 @@ $lang['enable_label']					= 'Enable';
 $lang['disable_label'] 					= 'Disable';
 $lang['upgrade_label'] 					= 'Upgrade';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts']	 			= 'Nuoroda';
 $lang['global:description'] 		    = 'Aprašymas';
 $lang['global:actions']					= 'Veiksmai';

system/cms/language/polish/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label']				= 'Instaluj';
 $lang['enable_label']				= 'Włącz';
 $lang['disable_label']				= 'Wyłącz';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powred_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts']	 			= 'Skróty klawiszowe';
 $lang['global:description']				= 'Opis';
 $lang['global:actions']					= 'Akcje';

Akshay-Hegde/honolii-cluster

system/cms/language/brazilian/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label'] 					= 'Instalar';
 $lang['enable_label']					= 'Ativar';
 $lang['disable_label'] 					= 'Desativar';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= 'Atalhos';
 $lang['global:description'] 		    = 'Descrição';
 $lang['global:actions']					= 'Ações';

system/cms/language/chinese_traditional/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label'] 					= '安裝'; 			//'Install';
 $lang['enable_label']					= '啟用'; 			//'Enable';
 $lang['disable_label'] 					= '禁用'; 			//'Disable';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= '捷徑';
 $lang['global:description']				= '說明';
 $lang['global:actions']					= '操作';

system/cms/language/english/global_lang.php
@@ -69,6 +69,11 @@ $lang['enable_label']					= 'Enable';
 $lang['disable_label'] 					= 'Disable';
 $lang['upgrade_label'] 					= 'Upgrade';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';
+$lang['top_page']						= 'Top';
+$lang['back_to_top']					= 'Back to Top';
+
 $lang['global:filters'] 				= 'Filters';
 $lang['global:shortcuts'] 				= 'Shortcuts';
 $lang['global:description']				= 'Description';

system/cms/language/german/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label'] 					= 'Installieren';
 $lang['enable_label']					= 'Aktivieren';
 $lang['disable_label'] 					= 'Deaktivieren';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= 'Shortcuts'; #translate 
 $lang['global:description']				= 'Beschreibung'; 
 $lang['global:actions']					= 'Aktion';

system/cms/language/greek/global_lang.php
@@ -68,6 +68,11 @@ $lang['enable_label']					= 'Ενεργοποίηση';
 $lang['disable_label'] 					= 'Απενεργοποίηση';
 $lang['upgrade_label'] 					= 'Ενημέρωση';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:filters'] 				= 'Φίλτρα';
 $lang['global:shortcuts'] 				= 'Συντομέυσεις';
 $lang['global:description'] 		    = 'Περιγραφή';

system/cms/language/italian/global_lang.php
@@ -67,6 +67,11 @@ $lang['enable_label']					= 'Abilita';
 $lang['disable_label'] 					= 'Disabilita';
 $lang['upgrade_label'] 					= 'Aggiorna';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts'] 				= 'Collegamenti';
 $lang['global:description'] 		    = 'Descrizione';
 $lang['global:actions']					= 'Azioni';

system/cms/language/lithuanian/global_lang.php
@@ -67,6 +67,11 @@ $lang['enable_label']					= 'Enable';
 $lang['disable_label'] 					= 'Disable';
 $lang['upgrade_label'] 					= 'Upgrade';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts']	 			= 'Nuoroda';
 $lang['global:description'] 		    = 'Aprašymas';
 $lang['global:actions']					= 'Veiksmai';

system/cms/language/polish/global_lang.php
@@ -66,6 +66,11 @@ $lang['install_label']				= 'Instaluj';
 $lang['enable_label']				= 'Włącz';
 $lang['disable_label']				= 'Wyłącz';
 
+$lang['all_right_reserved']				= 'All Rights Reserved.';  #translate
+$lang['powered_by_pyrocms']				= 'Powered by PyroCMS';  #translate
+$lang['top_page']						= 'Top';  #translate
+$lang['back_to_top']					= 'Back to Top';  #translate
+
 $lang['global:shortcuts']	 			= 'Skróty klawiszowe';
 $lang['global:description']				= 'Opis';
 $lang['global:actions']					= 'Akcje';