aa916ddeb9dc4fc03a4fc0eef87e946fa7f10405
Adam Fairholm <adam.fairholm@gmail.com>
2012-02-04 20:43:46 -0500

    Updating to latest version of PyroStreams - 2.2-dev. This version is designed to work with streams_core.

---
+++ system/cms/modules/streams/js/fields.js
@@ -0,0 +1,26 @@
+function add_field_parameters()
+{
+	var data = document.getElementById("field_type").value;
+	
+	jQuery.ajax({
+		dataType: "text",
+		type: "POST",
+		data: 'data='+data,
+		url:  BASE_URL+'index.php/streams_core/ajax/build_parameters',
+		success: function(returned_html){
+			jQuery('.streams_param_input').remove();
+			jQuery('.form_inputs > ul').append(returned_html);
+			pyro.chosen();
+		}
+	});
+}
+
+jQuery(document).ready(function() {
+
+	$('#field_name').keyup(function() {
+  
+ 	 	$('#field_slug').val(slugify($('#field_name').val()));
+ 	   
+	});
+
+});
