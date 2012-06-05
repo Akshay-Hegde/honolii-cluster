18f5d7a536914cf85d4096643ea473eb842c2e8e
Edward Meehan <edward.meehan.808@gmail.com>
2012-06-04 23:11:06 -0700

    lesscompile plugin fix

--- system/cms/core/MY_Loader.php
+++ system/cms/core/MY_Loader.php
@@ -19,9 +19,10 @@ class MY_Loader extends MX_Loader {
             define('SPARKPATH', 'system/sparks/');
         }
 		
+		parent::__construct();
+		
 		$this->add_package_path(SHARED_ADDONPATH);
 		
-		parent::__construct();
 	}
 
 	/**
