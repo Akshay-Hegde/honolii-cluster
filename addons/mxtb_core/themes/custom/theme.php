254fece54d697888e183e5780276944e76ed1e47
Edward Meehan <edward.meehan.808@gmail.com>
2012-08-29 22:53:55 -0700

    Added US Techline site Added build for ustechline template Added build for mxtrackbuild template

---
+++ addons/mxtb_core/themes/custom/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Custom extends Theme {
+
+    public $name = 'Rebound';
+    public $author = 'Edward Meehan - Wetumka';
+    public $author_website = 'http://www.wetumka.net/';
+    public $website = 'http://www.pyrocms.com/';
+    public $description = 'Custom Theme built for a blog site.';
+    public $version = '1.0';
+
+}
+
+/* End of file theme.php */
