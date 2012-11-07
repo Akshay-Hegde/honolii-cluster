2ea197e40a73ce9e4318800ae6379fdc8e035c22
Edward Meehan <edward.meehan.808@gmail.com>
2012-11-06 21:37:55 -0800

    update to less library

--- addons/mxtb_core/themes/custom/theme.php
+++ addons/mxtb_core/themes/custom/theme.php
@@ -2,11 +2,11 @@
 
 class Theme_Custom extends Theme {
 
-    public $name = 'Rebound';
+    public $name = 'MX Track Builder 2.0';
     public $author = 'Edward Meehan - Wetumka';
     public $author_website = 'http://www.wetumka.net/';
     public $website = 'http://www.pyrocms.com/';
-    public $description = 'Custom Theme built for a blog site.';
+    public $description = 'Custom Theme built for MX Track Builders, LLC.';
     public $version = '1.0';
 
 }
