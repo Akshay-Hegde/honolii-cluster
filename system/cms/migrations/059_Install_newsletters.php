5cbac1c1b265e904fe0624b9d3db414af7ee9a41
jerel <mail@unruhdesigns.com>
2012-12-12 23:15:23 -0600

    Removed newsletter leftovers

--- system/cms/migrations/059_Install_newsletters.php
+++ system/cms/migrations/059_Install_newsletters.php
@@ -4,18 +4,6 @@ class Migration_Install_newsletters extends CI_Migration {
 
 	public function up()
 	{
-		$this->load->model('modules/module_m');
-		
-		if ( ! $this->db->table_exists('newsletters'))
-		{
-			$this->module_m->install('newsletters', true);
-		}
-		else
-		{
-			$this->module_m->upgrade('newsletters');
-			$this->module_m->update('modules', array('is_core' => 1));
-		}
-
 		return true;
 	}
 
