b16c53e478a793aeabb65ab4bca71addd58d7671
jerel <mail@unruhdesigns.com>
2013-01-28 19:20:30 -0600

    Removed duplicate buttons on the installer complete page

--- installer/views/complete.php
+++ installer/views/complete.php
@@ -17,13 +17,10 @@
 <p><?php echo lang('outro_text'); ?></p>
 
 <p>
-	<?php echo anchor($website_url, lang('go_website'), 'class="button go_to_site"'); ?> 
+	<?php echo anchor($website_url, lang('go_website'), 'class="button go_to_site"'); ?>
 	<?php echo anchor($control_panel_url, lang('go_control_panel'), 'class="button go_to_site"'); ?>
+	<?php echo anchor($multi_site_url, lang('go_multi_site'), 'class="button go_to_site"'); ?>
 </p>
-
-<?php echo anchor($website_url, lang('go_website'), 'class="button go_to_site"'); ?>
-<?php echo anchor($control_panel_url, lang('go_control_panel'), 'class="button go_to_site"'); ?>
-<?php echo anchor($multi_site_url, lang('go_multi_site'), 'class="button go_to_site"'); ?>
 
 <script>
 	$(function(){
