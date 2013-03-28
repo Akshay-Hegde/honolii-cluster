2a8cd4a7a476e5de81e17cbcb5f56d413065ea15
Edward Meehan <edward.meehan.808@gmail.com>
2013-03-28 01:18:54 -0700

    updated emeehan site to 2.2 fixed admin error with ckedit.js updated snippets module - replace soon with streams

--- addons/emeehan/themes/custom/views/modules/blog/archive.php
+++ addons/emeehan/themes/custom/views/modules/blog/archive.php
@@ -1,32 +1,50 @@
-<h2 id="page_title"><?php echo lang('blog_archive_title');?></h2>
-<h3><?php echo $month_year;?></h3>
-<?php if (!empty($blog)): ?>
-<?php foreach ($blog as $post): ?>
-	<div class="blog_post">
-		<!-- Post heading -->
-		<div class="post_heading">
-			<h2><?php echo  anchor('blog/' .date('Y/m', $post->created_on) .'/'. $post->slug, $post->title); ?></h2>
-			<p class="post_date"><?php echo lang('blog_posted_label');?>: <?php echo format_date($post->created_on); ?></p>
-			<?php if($post->category_slug): ?>
-			<p class="post_category">
-				<?php echo lang('blog_category_label');?>: <?php echo anchor('blog/category/'.$post->category_slug, $post->category_title);?>
-			</p>
-			<?php endif; ?>
-			<?php if($post->keywords): ?>
-			<p class="post_keywords">
-				<?php echo lang('blog_tagged_label');?>:
-				<?php echo $post->keywords; ?>
-			</p>
-			<?php endif; ?>
-		</div>
-		<div class="post_body">
-			<?php echo $post->intro; ?>
-		</div>
-	</div>
-<?php endforeach; ?>
-
-<?php echo $pagination['links']; ?>
-
-<?php else: ?>
-	<p><?php echo lang('blog_currently_no_posts');?></p>
-<?php endif; ?>
+<section id="col-main" class="span12">
+    <h2 id="page_title">{{ helper:lang line="blog:archive_title" }}</h2>
+    <h3>{{ month_year }}</h3>
+    {{ if posts }}
+        {{ posts }}
+            {{ if category }}
+            <div class="mod block-post {{ category:slug }}-post">
+            {{ else }}
+            <div class="mod block-post nocat-post">
+            {{ endif }}
+                <div class="hd post-heading">
+                    <h4><a href="{{ url }}">{{ title }}</a></h4>
+                    <div class="post-date" title="{{ helper:date timestamp=created }}">
+                        <span class="post-month">{{ helper:date timestamp=created_on format="F" }}</span>
+                        <span class="post-day">{{ helper:date timestamp=created_on format="j" }}</span>
+                    </div>
+                    <div class="post-meta"> 
+                        {{ if category }}
+                        <span class="post-category">
+                            <i class="icon-bookmark icon-white"></i>
+                            <a href="blog/category/{{ category:slug }}">{{ category:title }}</a>
+                        </span>
+                        {{ endif }}
+                        {{ if keywords }}
+                        <span class="post-keywords">
+                            <i class="icon-tags icon-white"></i>
+                            {{ keywords }}
+                                <a href="blog/tagged/{{ keyword }}">{{ keyword }}</a>
+                            {{ /keywords }}
+                        </span>
+                        {{ endif }}
+                    </div>
+                </div>
+                <div class="bd post-body">
+                    {{ preview }}
+                    <p><a href="{{ url }}">{{ helper:lang line="blog:read_more_label" }}</a></p>
+                </div>
+            </div>
+        {{ /posts }}
+        {{ pagination }}
+    {{ else }}
+        {{ helper:lang line="blog:currently_no_posts" }}
+    {{ endif }}
+</section>
+<aside id="col-rail" class="span11 offset1">
+    <div class="row">
+        <div class="span8">{{ widgets:area slug="category" }}{{ widgets:area slug="default" }}</div>
+        <div class="span3">&nbsp;</div>
+    </div>
+</aside>
