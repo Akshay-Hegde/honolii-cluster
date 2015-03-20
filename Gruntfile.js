adfea1cd97339be722f87301a84ad8d931252a62
Edward Meehan <edward.meehan@wetumka.net>
2015-03-20 09:51:02 -1000

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,100 +3,78 @@ module.exports = function(grunt) {
     var target = grunt.option('env') || 'dev';
     var tasks;
 
-    // 1. All configuration goes here 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            dist: {
-                src: [
-                    'addons/emeehan/themes/custom/js/modules/*.js',
-                    'addons/emeehan/themes/custom/js/mylibs/*.js',
-                    'addons/emeehan/themes/custom/js/default.js'
-                ],
-                dest: 'production.js',
-            }
-        },
-        uglify: {
-            build: {
-                src: 'production.js',
-                dest: 'production.min.js'
-            }
-        },
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'addons/emeehan/themes/custom/img',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'build/'
-                }]
-            }
-        },
-        // cachebust: {
-        //     build: {
-        //         files: {
-        //             expand: true,
-        //             //cwd: 'test',
-        //             src: ['test.html'],
-        //             dest: 'test/'
-        //         }
-        //     }
-        // }
-        // asset_cachebuster: {
-        //     options: {
-        //         //buster:
-        //         //htmlExtension:
-        //         //ignore:
-        //     },
-        //     build: {
-        //         files: {
-        //             src: ['test.html'],
-        //             dest: ['test/test.html']
-        //         }
-        //     }
-        // }
-        cacheBust: {
-            options: {
-              encoding: 'utf8',
-              algorithm: 'md5',
-              length: 8,
-              deleteOriginals: true
-            },
-            assets: {
-              files: [
-                  {
-                    expand: true,
-                    cwd: 'test/',
-                    src: ['*.html'],
-                    dest: 'test/'
-                  }
-              ]
-            }
-        },
-        clean: {
-            build: {
-                noWrite: true,
-                src: 'test/'
-            }
+    grunt.initConfig({});
+    // CONCAT
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.config('concat',{
+        build: {
+            src: [
+                'addons/emeehan/themes/custom/js/modules/*.js',
+                'addons/emeehan/themes/custom/js/mylibs/*.js',
+                'addons/emeehan/themes/custom/js/default.js'
+            ],
+            dest: 'test/production.js',
         }
-
+    });
+    // UGLIFY
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.config('uglify',{
+        build: {
+            src: 'test/production.js',
+            dest: 'test/production.min.js'
+        }
+    });
+    // IMAGEMIN
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.config('imagemin',{
+        build: {
+            files: [{
+                expand: true,
+                cwd: 'addons/emeehan/themes/custom/img',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'build/'
+            }]
+        }
+    });
+    // CLEAN
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.config('clean',{
+        build: {
+            src: 'test/'
+        }
+    });
+    // CACHE BUST
+    grunt.loadNpmTasks('grunt-cache-bust');
+    grunt.config('cacheBust',{
+        options: {
+            encoding: 'utf8',
+            algorithm: 'md5',
+            length: 8,
+            deleteOriginals: true
+        },
+        build: {
+            files: [{
+                expand: true,
+                cwd: 'test/',
+                src: '*.html'
+            }]
+        }
+    });
+    // COPY
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.config('copy',{
+        build: {
+            expand: true,
+            cwd: 'test-pt2/',
+            src: '*.html',
+            dest: 'test/'
+        }
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-cache-bust');
-    //grunt.loadNpmTasks('grunt-cachebust');
-    //grunt.loadNpmTasks('grunt-asset-cachebuster');
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    //grunt.registerTask('default', ['concat', 'uglify', 'cacheBust']);
     if (target === 'prod') {
-        tasks = ['concat', 'uglify', 'clean', 'cacheBust'];
+        tasks = ['clean', 'copy', 'concat', 'uglify', 'cacheBust'];
     }else{
-        tasks = ['concat', 'uglify', 'clean', 'cacheBust'];
+        tasks = ['clean', 'copy', 'concat', 'uglify', 'cacheBust'];
     }
     
     grunt.registerTask('default', tasks);
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Honolii Cluster - PyroCMS Pro
-
+This application is the CMS for a few managed websites.
+* emeehan.com - Edward Meehan
+* autowrapshawaii.com - John Allen Sign Company
+* johnallensigncompany.com - John Allen Sign Company
+* northcountyeyesurgery.com - North County Eye Surgery
+* mxtrackbuilders.com - MX Track Builders
+* wetumka.net - Wetumka Interactive
 ## Getting Started
 In order to start development on this project you will need to install a few things on your local computer.
+* VM Software:
+** (VMware)[http://www.vmware.com/] - Fusion requires a (vagrant plugin to work)[http://www.vagrantup.com/vmware].
+** (Virtual Box)[https://www.virtualbox.org/] - Free alternative to Fusion, just not as powerful.
+* (Vagrant)[https://www.vagrantup.com/] - builds VM envrionment for development with a single command
